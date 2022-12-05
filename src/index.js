11fafd82267cfb753ae970b1959a120e46901ba1
khcjump <khcjump@gmail.com>
2022-12-05 23:41:10 +0800

    preparation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./style.css";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +8,11 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const btn = document.createElement("button");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
